40282de777ebf0fc64cc06b85353b581ad4aba66
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-01-29 23:27:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ListMaster;
+
+namespace ConsoleArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayMaster am = new();
+            // Решение задания 61 - найти произведение двух матриц
+            var firstArray = am.CreateTwoDimentionalArray(3, 4);
+            am.FillTwoDimentionalArray(firstArray, 0, 3);
+            am.PrintTwoDimentionalArray(firstArray);
+            Console.WriteLine();
+            var secondArray = am.CreateTwoDimentionalArray(4, 5);
+            am.FillTwoDimentionalArray(secondArray, 0, 9);
+            am.PrintTwoDimentionalArray(secondArray);
+            var resultArray = am.MultiplicationArrays(firstArray, secondArray);
+            am.PrintTwoDimentionalArray(resultArray);
+        }
+    }
+}
---
+++ ListMaster/ArrayMaster.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListMaster
+{
+    public class ArrayMaster
+    {
+        public int[,] CreateTwoDimentionalArray(int rows, int colums)
+        {
+            int[,] array = new int[rows, colums];
+            return array;
+        }
+        public void FillTwoDimentionalArray(int[,] array, int minNumber, int maxNumber)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(minNumber, maxNumber + 1);
+                }
+            }
+        }
+        public void PrintTwoDimentionalArray(int[,] array)
+        {
+            Console.Write("\t");
+            for (int j = 0; j < array.GetLength(1); j++) Console.Write($"{j}\t");
+            Console.WriteLine();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write($"{i}\t");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public int[,] MultiplicationArrays(int[,] firstArray, int[,] secondArray)
+        {
+            int firstArrayRows = firstArray.GetLength(0);
+            int firstArrayColumns = firstArray.GetLength(1);
+            int secondArrayRows = secondArray.GetLength(0);
+            int secondArrayColumns = secondArray.GetLength(1);
+            if (firstArrayColumns != secondArrayRows) return new int[0, 0];
+
+            int[,] resultArray = new int[firstArrayRows, secondArrayColumns];
+
+            for (int i = 0; i < resultArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < resultArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < firstArray.GetLength(1); k++)
+                    {
+                        resultArray[i, j] += firstArray[i, k] * secondArray[k, j];
+                    }
+                }
+            }
+            return resultArray;
+        }
+    }
+}
